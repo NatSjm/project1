9768802318fa7940bf8090dcc5bf815bf1c7cc7f
Oleg <oleh.vysotskyi@gmail.com>
2019-04-15 18:46:25 +0300

    -fixes

--- resources/views/pages/layouts/layout.blade.php
+++ resources/views/pages/layouts/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Laravel</title>
-    <link href="{{ URL::asset('css/app.css') }}" rel= "stylesheet" type="text/css">
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet" type="text/css">
 </head>
 <body>
 
