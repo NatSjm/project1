f813841ed4c8b59c168684b2c70381e97cd75006
Nat Sjm <dlyagame78@gmail.com>
2019-05-09 23:35:10 +0300

    #search_breadcrumbs @layouts -fix

--- resources/views/components/breadcrumbs/breadcrumbs.blade.php
+++ resources/views/components/breadcrumbs/breadcrumbs.blade.php
@@ -2,6 +2,6 @@
     <li class="breadcrumbs_item"><a href="#">Главная</a></li>
     <li class="breadcrumbs_item"><a href="#">{{ $crumb_level2 }}</a></li>
     @isset($crumb_level3)
-    <li class="breadcrumbs_item"><a href="#">{{ $crumb_level3 }}</a></li>
-        @endisset
+        <li class="breadcrumbs_item"><a href="#">{{ $crumb_level3 }}</a></li>
+    @endisset
 </ul>
