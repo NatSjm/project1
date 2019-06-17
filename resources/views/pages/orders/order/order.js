06204d79470a13cb76fe9141a67ddbaeba0f64a1
Nat Sjm <dlyagame78@gmail.com>
2019-06-17 07:52:50 +0300

    #orders @scripts -fix Close not active orders

--- resources/views/pages/orders/order/order.js
+++ resources/views/pages/orders/order/order.js
@@ -1,9 +1,23 @@
-var switcher = $('.js_details-switcher');
+// var switcher = $('.js_details-switcher');
+// switcher.click(function(e){
+//     e.preventDefault();
+//     let order =  $(this).closest('.order');
+//     order.toggleClass('mod_opened');
+//     order.next().find('.order_items-wrapper').slideToggle();
+//
+// });
+
+var switcher = $('.order');
 switcher.click(function(e){
-    e.preventDefault();
-    let order =  $(this).closest('.order');
-    order.toggleClass('mod_opened');
-    order.next().find('.order_items-wrapper').slideToggle();
+    let others = switcher.not($(this));
+
+    if (others.hasClass('mod_opened')){
+        others.removeClass('mod_opened');
+        others.next().find('.order_items-wrapper').slideUp();
+    }
+
+    $(this).toggleClass('mod_opened');
+    $(this).next().find('.order_items-wrapper').slideToggle();
 
 });
 
