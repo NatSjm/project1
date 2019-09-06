980e2f446d8ccbb20f78df5db205bdad7872e803
Nat Sjm <dlyagame78@gmail.com>
2019-09-06 19:04:06 +0300

    person-page @layouts -fix avatar height equals width

--- resources/views/pages/person/person.js
+++ resources/views/pages/person/person.js
@@ -12,4 +12,20 @@ document.querySelector(".person_img").addEventListener("change", function () {
         fr.readAsDataURL(this.files[0]);
     }
 });
-l
+
+
+let avatar = $('.person_img-title');
+
+function  avatarResiser() {
+    if (window.matchMedia("screen and (max-width: 1279px)").matches) {
+        avatar.css("width", "100%");
+        avatar.height(avatar.width());
+    } else {
+       avatar.height(180);
+       avatar.width(180);
+    }
+}
+
+avatarResiser();
+
+$(window).resize(avatarResiser);
